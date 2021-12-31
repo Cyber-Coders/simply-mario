f4142c12f4f59499637f331f9fe0ef9bfea6077e
Osman <61325807+osmanof@users.noreply.github.com>
2021-12-31 19:04:43 +0300

    edited main.py

---
+++ config.py
@@ -0,0 +1,3 @@
+SIZE = W, H = 1280, 720
+TITLE = "SimplyMario"
+FPS = 30
---
+++ main.py
@@ -0,0 +1,31 @@
+import sys
+
+import config
+import pygame
+
+
+class Game:
+    def __init__(self):
+        self.size = self.width, self.height = config.SIZE
+        self.screen = pygame.display.set_mode(self.size)
+
+    def update(self):
+        pass
+
+
+if __name__ == "__main__":
+    pygame.init()
+    pygame.display.set_caption(config.TITLE)
+
+    game = Game()
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit(0)
+
+        game.update()
+        clock.tick(config.FPS)
+
+        pygame.display.flip()
