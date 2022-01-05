2366b556dc360099854606b3685f14b199ea855a
User <muhammag11@gmail.com>
2022-01-05 16:43:55 +0300

    Add button "Continue"

--- menu.py
+++ menu.py
@@ -7,9 +7,45 @@ import time
 from main import Game
 
 
+def game_plot():  # Сюжет игры
+    screen = pygame.display.set_mode((1280, 720))
+    manager = pygame_gui.UIManager((1280, 720), 'theme.json')
+    clock = pygame.time.Clock()
+    pygame.mixer.music.pause()
+    count = 1
+    running = True
+    image = pygame.image.load('data/sprites/starter_template.tif')
+    image_2 = pygame.image.load('data/sprites/s_1.png')
+    button_continue = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((1010, 650), (260, 55)), text='Continue',
+                                                   manager=manager)
+
+    while running:
+        time_delta = clock.tick(60) / 1000.0
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit(0)
+
+            if event.type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element == button_continue:
+                    count += 1
+
+            manager.process_events(event)
+        manager.update(time_delta)
+        if count == 1:
+            screen.blit(image, (0, 0))
+
+        if count == 2:
+            screen.blit(image_2, (0, 0))
+
+        manager.draw_ui(screen)
+        pygame.display.flip()
+
+
 def window_records():  # Раздел рекордов
     screen = pygame.display.set_mode((1280, 720))
     manager = pygame_gui.UIManager((1280, 720), 'theme.json')
+    pygame.mixer.music.stop()
     clock = pygame.time.Clock()
     running = True
     image = pygame.image.load('data/sprites/records.png')
@@ -106,7 +142,7 @@ class Menu(Game):  # Главное меню
 
                 if event.type == pygame_gui.UI_BUTTON_PRESSED:
                     if event.ui_element == self.button_start:
-                        self.start_game()
+                        game_plot()
 
                     if event.ui_element == self.button_quit:
                         running = False
