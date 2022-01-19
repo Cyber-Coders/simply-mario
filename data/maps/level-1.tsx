f12a6fb0c3e4e4925c5304e18b0bde96aaa60354
osman <lyceum.yandex@gmail.com>
2022-01-19 23:20:24 +0300

    added map

---
+++ data/maps/level-1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="level-1" tilewidth="20" tileheight="20" tilecount="1200" columns="40">
+ <image source="tilemap.png" trans="000000" width="800" height="600"/>
+</tileset>
