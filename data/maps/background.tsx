0f7d7c4bccec3251235de34b0261199797d9d6b9
osman <lyceum.yandex@gmail.com>
2022-01-20 22:50:06 +0300

    created a more beautiful map

--- data/maps/background.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="background" tilewidth="20" tileheight="20" tilecount="72000" columns="2000">
- <image source="background.jpg" width="40000" height="720"/>
-</tileset>
--- data/maps/trees.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="trees" tilewidth="20" tileheight="20" tilecount="216" columns="24">
- <image source="trees.png" width="480" height="198"/>
-</tileset>
